9bf32733b7666d7cfc85a5d7cd15c3ef85bf4950
Aditi Anand <aditi.anand@gsa.gov>
2024-04-18 15:44:16 -0400

    LG-11820: cleaning up teh code

--- config/initializers/ext_active_record_redshift_adapter.rb
+++ config/initializers/ext_active_record_redshift_adapter.rb
@@ -10,12 +10,6 @@ module IdentityReporting
     # separately than keyword options.
     # This monkeypatch accepts either form
     def create_database(name, positional_options = {}, **keyword_options)
-      positional_options_transformed = positional_options.transform_values do |val|
-        val.is_a?(String) || val.is_a?(Array) ? val.last(5) : val
-      end
-
-      puts "name: #{name}"
-      puts "positional_options: #{positional_options_transformed.inspect}"
       super(name, **positional_options.merge(keyword_options).transform_keys(&:to_sym))
     end
   end
