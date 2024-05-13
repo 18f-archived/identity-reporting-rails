be27bdbbc70ddbb9c4f6f4dfe7a4451f090f56ad
Aditi Anand <aditi.anand@gsa.gov>
2024-05-13 15:05:37 -0400

    changes to fix the lint error "Rails/ReversibleMigration: execute is not reversible."

--- db/data_warehouse_test_migrate/20240508161557_create_events.rb
+++ db/data_warehouse_test_migrate/20240508161557_create_events.rb
@@ -1,6 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[7.1]
   def change
-    execute 'CREATE SCHEMA IF NOT EXISTS logs'
+    reversible do |dir|
+      dir.up { execute 'CREATE SCHEMA IF NOT EXISTS logs' }
+      dir.down { execute 'DROP SCHEMA IF EXISTS logs' }
+    end
 
     create_table 'logs.events', if_not_exists: true, id: false do |t|
       t.text :message
--- db/data_warehouse_test_migrate/20240508162648_create_articles.rb
+++ db/data_warehouse_test_migrate/20240508162648_create_articles.rb
@@ -1,6 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[7.1]
   def change
-    execute 'CREATE SCHEMA IF NOT EXISTS idp'
+    reversible do |dir|
+      dir.up { execute 'CREATE SCHEMA IF NOT EXISTS idp' }
+      dir.down { execute 'DROP SCHEMA IF EXISTS idp' }
+    end
 
     create_table 'idp.articles', if_not_exists: true, id: false do |t|
       t.integer :id, primary_key: false
