98bf997047ce72e3d01381ea19545373b35db07f
Aditi Anand <aditi.anand@gsa.gov>
2024-05-13 17:46:56 -0400

    changes for data_warehouse connection

--- spec/jobs/duplicate_row_checker_job_spec.rb
+++ spec/jobs/duplicate_row_checker_job_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe DuplicateRowCheckerJob, type: :job do
   let(:idp_job) { DuplicateRowCheckerJob.new }
   let(:logs_job) { DuplicateRowCheckerJob.new }
 
+  before do
+    ActiveRecord::Base.establish_connection(:data_warehouse)
+  end
+
   describe '#perform' do
     context 'when there are duplicate articles' do
       before do
