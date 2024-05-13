a4b4e5f3e2d207707598749ad50c4920945d75b7
Aditi Anand <aditi.anand@gsa.gov>
2024-05-13 14:39:37 -0400

    added a spec for the factory

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    article = FactoryBot.build(:article)
+    expect(article).to be_valid
+  end
 end
