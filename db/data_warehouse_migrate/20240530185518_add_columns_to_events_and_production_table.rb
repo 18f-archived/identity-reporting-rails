afefcc64c7ccb6a9fc2b330b63049de873efbb0c
Osman Latif <olatif@flexion.us>
2024-05-31 16:41:59 -0500

    LG-13365: Events and Productions table migrations

---
+++ db/data_warehouse_migrate/20240530185518_add_columns_to_events_and_production_table.rb
@@ -0,0 +1,49 @@
+class AddColumnsToEventsAndProductionTable < ActiveRecord::Migration[7.1]
+  def change
+    # Add columns to 'logs.events' table
+    add_column 'logs.events', :id, :string
+    add_column 'logs.events', :name, :string
+    add_column 'logs.events', :time, :timestamp
+    add_column 'logs.events', :visitor_id, :string
+    add_column 'logs.events', :visit_id, :string
+    add_column 'logs.events', :log_filename, :string
+    add_column 'logs.events', :new_event, :boolean, default: false, null: false
+    add_column 'logs.events', :path, :string
+    add_column 'logs.events', :user_id, :string
+    add_column 'logs.events', :locale, :string
+    add_column 'logs.events', :user_ip, :string
+    add_column 'logs.events', :hostname, :string
+    add_column 'logs.events', :pid, :integer
+    add_column 'logs.events', :service_provider, :string
+    add_column 'logs.events', :trace_id, :string
+    add_column 'logs.events', :git_sha, :string
+    add_column 'logs.events', :git_branch, :string
+    add_column 'logs.events', :user_agent, :string
+    add_column 'logs.events', :browser_name, :string
+    add_column 'logs.events', :browser_version, :string
+    add_column 'logs.events', :browser_platform_name, :string
+    add_column 'logs.events', :browser_platform_version, :string
+    add_column 'logs.events', :browser_device_name, :string
+    add_column 'logs.events', :browser_mobile, :boolean, default: false, null: false
+    add_column 'logs.events', :browser_bot, :boolean, default: false, null: false
+    add_column 'logs.events', :success, :boolean, default: false, null: false
+
+    # Add columns to 'logs.production' table
+    add_column 'logs.production', :uuid, :string
+    add_column 'logs.production', :method, :string
+    add_column 'logs.production', :path, :string
+    add_column 'logs.production', :format, :string
+    add_column 'logs.production', :controller, :string
+    add_column 'logs.production', :action, :string
+    add_column 'logs.production', :status, :integer
+    add_column 'logs.production', :duration, :float
+    add_column 'logs.production', :git_sha, :string
+    add_column 'logs.production', :git_branch, :string
+    add_column 'logs.production', :timestamp, :timestamp
+    add_column 'logs.production', :pid, :integer, null: true
+    add_column 'logs.production', :user_agent, :string
+    add_column 'logs.production', :ip, :string
+    add_column 'logs.production', :host, :string
+    add_column 'logs.production', :trace_id, :string
+  end
+end
