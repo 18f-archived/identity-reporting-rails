183ac279d12e453d2358a9237ab45daf3a682375
Osman Latif <olatif@flexion.us>
2024-06-10 16:59:09 -0500

    LG-13365: Events and production tables migrations updates

--- db/data_warehouse_migrate/20240530181654_create_events_and_production_table.rb
+++ db/data_warehouse_migrate/20240530181654_create_events_and_production_table.rb
@@ -5,44 +5,23 @@ class CreateEventsAndProductionTable < ActiveRecord::Migration[7.1]
       dir.down { execute 'DROP SCHEMA IF EXISTS logs' }
     end
 
-    # Check if logs.unextracted_events table exists, if not, create it
-    create_table 'logs.unextracted_events', if_not_exists: true, id: false do |t|
-      if connection.adapter_name.downcase.include? 'redshift'
-        t.column :message, :super
-      else
-        t.jsonb :message
-      end
-      t.timestamp :cloudwatch_timestamp
-    end
+    # Define the tables to be created
+    tables = ['unextracted_events', 'unextracted_production', 'events', 'production']
 
-    # Check if logs.unextracted_production table exists, if not, create it
-    create_table 'logs.unextracted_production', if_not_exists: true, id: false do |t|
-      if connection.adapter_name.downcase.include? 'redshift'
-        t.column :message, :super
+    tables.each do |table|
+      if connection.adapter_name.downcase.include?('redshift')
+        execute <<-SQL
+          CREATE TABLE IF NOT EXISTS logs.#{table} (
+            message SUPER,
+            cloudwatch_timestamp TIMESTAMP
+          );
+        SQL
       else
-        t.jsonb :message
+        create_table "logs.#{table}", if_not_exists: true, id: false do |t|
+          t.jsonb :message
+          t.timestamp :cloudwatch_timestamp
+        end
       end
-      t.timestamp :cloudwatch_timestamp
-    end
-
-    # Check if logs.events table exists, if not, create it
-    create_table 'logs.events', if_not_exists: true, id: false do |t|
-      if connection.adapter_name.downcase.include? 'redshift'
-        t.column :message, :super
-      else
-        t.jsonb :message
-      end
-      t.timestamp :cloudwatch_timestamp
-    end
-
-    # Check if logs.production table exists, if not, create it
-    create_table 'logs.production', if_not_exists: true, id: false do |t|
-      if connection.adapter_name.downcase.include? 'redshift'
-        t.column :message, :super
-      else
-        t.jsonb :message
-      end
-      t.timestamp :cloudwatch_timestamp
     end
   end
 end
