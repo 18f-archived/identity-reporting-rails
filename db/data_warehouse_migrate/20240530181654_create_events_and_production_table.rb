6fc71d0d05b8250bebb3e6aebb43cffad93780a1
Osman Latif <olatif@flexion.us>
2024-06-10 17:11:02 -0500

    cleanup

--- db/data_warehouse_migrate/20240530181654_create_events_and_production_table.rb
+++ db/data_warehouse_migrate/20240530181654_create_events_and_production_table.rb
@@ -5,23 +5,16 @@ class CreateEventsAndProductionTable < ActiveRecord::Migration[7.1]
       dir.down { execute 'DROP SCHEMA IF EXISTS logs' }
     end
 
-    # Define the tables to be created
     tables = ['unextracted_events', 'unextracted_production', 'events', 'production']
+    message_data_type = connection.adapter_name.downcase.include?('redshift') ? 'SUPER' : 'JSONB'
 
     tables.each do |table|
-      if connection.adapter_name.downcase.include?('redshift')
-        execute <<-SQL
-          CREATE TABLE IF NOT EXISTS logs.#{table} (
-            message SUPER,
-            cloudwatch_timestamp TIMESTAMP
-          );
-        SQL
-      else
-        create_table "logs.#{table}", if_not_exists: true, id: false do |t|
-          t.jsonb :message
-          t.timestamp :cloudwatch_timestamp
-        end
-      end
+      execute <<-SQL
+        CREATE TABLE IF NOT EXISTS logs.#{table} (
+          message #{message_data_type},
+          cloudwatch_timestamp TIMESTAMP
+        );
+      SQL
     end
   end
 end
