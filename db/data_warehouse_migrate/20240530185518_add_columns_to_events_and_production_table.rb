8563846fae01400546fad881e9e9ebbf64d6c8f8
Birane Seck <birane.seck@gsa.gov>
2024-06-12 15:35:19 -0400

    Update migration for log tables

--- db/data_warehouse_migrate/20240530185518_add_columns_to_events_and_production_table.rb
+++ db/data_warehouse_migrate/20240530185518_add_columns_to_events_and_production_table.rb
@@ -7,8 +7,8 @@ class AddColumnsToEventsAndProductionTable < ActiveRecord::Migration[7.1]
     add_column 'logs.events', :visitor_id, :string
     add_column 'logs.events', :visit_id, :string
     add_column 'logs.events', :log_filename, :string
-    add_column 'logs.events', :new_event, :boolean, default: false, null: false
-    add_column 'logs.events', :path, :string
+    add_column 'logs.events', :new_event, :boolean, null: true
+    add_column 'logs.events', :path, :string, limit: 12000
     add_column 'logs.events', :user_id, :string
     add_column 'logs.events', :locale, :string
     add_column 'logs.events', :user_ip, :string
@@ -18,20 +18,20 @@ class AddColumnsToEventsAndProductionTable < ActiveRecord::Migration[7.1]
     add_column 'logs.events', :trace_id, :string
     add_column 'logs.events', :git_sha, :string
     add_column 'logs.events', :git_branch, :string
-    add_column 'logs.events', :user_agent, :string
+    add_column 'logs.events', :user_agent, :string, limit: 12000
     add_column 'logs.events', :browser_name, :string
     add_column 'logs.events', :browser_version, :string
     add_column 'logs.events', :browser_platform_name, :string
     add_column 'logs.events', :browser_platform_version, :string
     add_column 'logs.events', :browser_device_name, :string
-    add_column 'logs.events', :browser_mobile, :boolean, default: false, null: false
-    add_column 'logs.events', :browser_bot, :boolean, default: false, null: false
-    add_column 'logs.events', :success, :boolean, default: false, null: false
+    add_column 'logs.events', :browser_mobile, :boolean, null: true
+    add_column 'logs.events', :browser_bot, :boolean, null: true
+    add_column 'logs.events', :success, :boolean, null: true
 
     # Add columns to 'logs.production' table
     add_column 'logs.production', :uuid, :string
     add_column 'logs.production', :method, :string
-    add_column 'logs.production', :path, :string
+    add_column 'logs.production', :path, :string, limit: 12000
     add_column 'logs.production', :format, :string
     add_column 'logs.production', :controller, :string
     add_column 'logs.production', :action, :string
@@ -41,7 +41,7 @@ class AddColumnsToEventsAndProductionTable < ActiveRecord::Migration[7.1]
     add_column 'logs.production', :git_branch, :string
     add_column 'logs.production', :timestamp, :timestamp
     add_column 'logs.production', :pid, :integer, null: true
-    add_column 'logs.production', :user_agent, :string
+    add_column 'logs.production', :user_agent, :string, limit: 12000
     add_column 'logs.production', :ip, :string
     add_column 'logs.production', :host, :string
     add_column 'logs.production', :trace_id, :string
