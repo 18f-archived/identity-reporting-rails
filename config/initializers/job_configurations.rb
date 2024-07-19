7fb5279281b6745818137cb3bac4ead4d421478a
samathad2023 <sdondeti@flexion.us>
2024-07-19 13:03:40 -0700

    log column extractor job

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -1,5 +1,7 @@
 cron_5m = '0/5 * * * *'
+cron_2m = '0/2 * * * *'
 cron_1d = '0 0 * * *'
+table_names = ['events', 'production']
 
 if defined?(Rails::Console)
   Rails.logger.info 'job_configurations: console detected, skipping schedule'
@@ -12,7 +14,7 @@ else
         cron: cron_5m,
       },
       # Queue logs column extractor job to GoodJob
-      log_tables_column_extractor_job: {
+      logs_column_extractor_job: {
         class: 'LogsColumnExtractorJob',
         cron: cron_1d,
       },
@@ -21,7 +23,16 @@ else
         class: 'DuplicateRowCheckerJob',
         cron: cron_1d,
       },
-    }
+    }.compact
+    # dynamic job scheduling
+    table_names.each do |table_name|
+      config.good_job.cron[:"logs_column_extractor_job_#{table_name}"] = {
+        class: 'LogsColumnExtractorJob',
+        cron: cron_2m,
+        args: -> { [table_name] },
+      }
+    end
   end
+
   Rails.logger.info 'job_configurations: jobs scheduled with good_job.cron'
 end
