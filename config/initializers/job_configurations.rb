fe12dd723f5a83efbacfc828818e487b1b8b0c23
samathad2023 <sdondeti@flexion.us>
2024-07-30 11:17:39 -0700

    job configuration

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -1,22 +1,11 @@
-require 'rake'
 require 'schema_table_service'
 
 cron_5m = '0/5 * * * *'
 cron_2m = '0/2 * * * *'
 cron_1d = '0 0 * * *'
-table_names = ['events', 'production']
 
-Rake.application.init
-Rake.application.load_rakefile
-
-# Rake::Task['db:schema_table_list'].invoke
 schema_table_service = SchemaTableService.new
 schema_table_hash = schema_table_service.generate_schema_table_hash
-# schema_table_hash.each do |schema_name, tables|
-#   tables.each do |table_name, uniq_by|
-#     Rake::Task["db:logs_column_extractor_job_#{table_name}"].invoke
-#   end
-# end
 
 if defined?(Rails::Console)
   Rails.logger.info 'job_configurations: console detected, skipping schedule'
