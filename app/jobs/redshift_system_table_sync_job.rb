d869ca785750e6c5602a324dda661d5961b806df
Osman Latif <109746710+olatifflexion@users.noreply.github.com>
2024-11-01 09:32:24 -0500

    job changes

--- app/jobs/redshift_system_table_sync_job.rb
+++ app/jobs/redshift_system_table_sync_job.rb
@@ -2,61 +2,49 @@ class RedshiftSystemTableSyncJob < ApplicationJob
   queue_as :default
 
   def perform
-    table_definitions = YAML.load_file(Rails.root.join('config/redshift_system_tables.yml'))['tables']
-
     table_definitions.each do |table|
-      unless target_table_exists?(table['target_table'])
-        create_target_table(table['target_table'], table['name'])
-      end
+      target_schema = table['target_schema']
+      source_table = table['name']
+      target_table = table['target_table']
+      target_table_with_schema = [target_schema, target_table].join('.')
+      timestamp_column = table['timestamp_column']
+      primary_key = table['primary_key']
 
-      last_sync_time = fetch_last_sync_time(table['name'])
-      new_data = fetch_recent_data(table['name'], table['timestamp_column'], last_sync_time)
+      create_target_table(source_table, target_table_with_schema)
 
-      if new_data.empty?
-        log_info(
-          'Missing data in source table',
-          false,
-          source_table_name: table['name'],
-          last_sync_time: last_sync_time,
-        )
-        next
-      end
+      last_sync_time = fetch_last_sync_time(target_table_with_schema)
+      new_data = fetch_recent_data(target_table_with_schema, timestamp_column, last_sync_time)
 
-      upsert_data(table['target_table'], table['primary_key'], new_data)
-      update_sync_time(table['name'])
-      log_info("Upserted data into #{table['target_table']}", true, record_count: new_data.size)
+      upsert_data(target_table_with_schema, primary_key, new_data)
+      update_sync_time(target_table_with_schema)
+      log_info("Upserted data into #{target_table_with_schema}", true, record_count: new_data.size)
     end
   end
 
   private
 
-  def log_info(message, success, additional_info = {})
-    Rails.logger.info(
-      {
-        job: self.class.name,
-        success: success,
-        message: message,
-      }.merge(additional_info).to_json,
-    )
+  def table_definitions
+    YAML.load_file(Rails.root.join('config/redshift_system_tables.yml'))['tables']
   end
 
   def target_table_exists?(table_name)
     DataWarehouseApplicationRecord.connection.table_exists?(table_name)
   end
 
-  def create_target_table(target_table, source_table)
+  def create_target_table(source_table, target_table_with_schema)
+    return if target_table_exists?(target_table_with_schema)
+
     columns = fetch_source_columns(source_table)
 
-    DataWarehouseApplicationRecord.connection.create_table(target_table) do |t|
+    DataWarehouseApplicationRecord.connection.create_table(target_table_with_schema, id: false) do |t|
       columns.each do |column_info|
-        column_name, column_data_type = column_info.values_at('name', 'datatype')
+        column_name, column_data_type = column_info.values_at('column_name', 'data_type')
         config_column_options = get_config_column_options(column_info)
-        next if column_name == 'id'
 
         t.column column_name, column_data_type, **config_column_options
       end
     end
-    log_info("Created target table #{target_table}", true, target_table: target_table)
+    log_info("Created target table #{target_table_with_schema}", true, target_table: target_table_with_schema)
   end
 
   def fetch_source_columns(source_table)
@@ -71,11 +59,6 @@ class RedshiftSystemTableSyncJob < ApplicationJob
 
   def get_config_column_options(_column_info)
     {}
-  end
-
-  def fetch_last_sync_time(table_name)
-    sync_metadata = SystemTableSyncMetadata.find_by(table_name: table_name)
-    sync_metadata ? sync_metadata.last_sync_time : (Time.zone.now - 5.days)
   end
 
   def fetch_recent_data(table_name, timestamp_column, last_sync_time)
@@ -105,10 +88,24 @@ class RedshiftSystemTableSyncJob < ApplicationJob
     end
   end
 
+  def fetch_last_sync_time(table_name)
+    sync_metadata = SystemTableSyncMetadata.find_by(table_name: table_name)
+    sync_metadata&.last_sync_time || (Time.zone.now - 6.days)
+  end
+
   def update_sync_time(table_name)
-    timestamp = Time.zone.now
     sync_metadata = SystemTableSyncMetadata.find_or_initialize_by(table_name: table_name)
-    sync_metadata.last_sync_time = timestamp
+    sync_metadata.last_sync_time = Time.zone.now
     sync_metadata.save!
+  end
+
+  def log_info(message, success, additional_info = {})
+    Rails.logger.info(
+      {
+        job: self.class.name,
+        success: success,
+        message: message,
+      }.merge(additional_info).to_json,
+    )
   end
 end
