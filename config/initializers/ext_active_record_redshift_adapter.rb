c0301c5e42e0391d61d3972d7232d831f13fe57a
aaron.nagucki <aaron.nagucki@gsa.gov>
2024-12-20 17:28:44 +0000

    Lg 15236/upgrade v2

--- config/initializers/ext_active_record_redshift_adapter.rb
+++ config/initializers/ext_active_record_redshift_adapter.rb
@@ -1,4 +1,4 @@
-if ActiveRecord.version >= Gem::Version.new('7.2.0')
+if ActiveRecord.version >= Gem::Version.new('7.2.2')
   warn 'Unexpected ActiveRecord version, double check that the constructor ' \
        'monkeypatch is still needed'
 end
